11b6a326182445e17ab8af938e25388732b06bc0
Mwovi Kelvin <ngusyakelvin@gmail.com>
2021-06-21 09:03:20 -0700

    prints the alphabet in lowercase, followed by a new line

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Entry point for script
+* Return: Success returns 0.
+*/
+
+int main(void)
+{
+int x;
+
+for (x = 'a'; x <= 'z'; x++)
+{
+putchar(x);
+}
+putchar('\n');
+return (0);
+}
