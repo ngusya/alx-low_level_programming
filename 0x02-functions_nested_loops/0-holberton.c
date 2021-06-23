1184ed7e7e3e93079829ec565b1d43388378c2fb
Mwovi Kelvin <ngusyakelvin@gmail.com>
2021-06-22 23:42:15 -0700

    second commit

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,21 +1,20 @@
 #include "holberton.h"
 /**
- * main - Entry block
- * Description: prints holberton on a new line
- * Return: 0
- */
-
-int main()
+* main - Entry block
+* Description: prints holberton on a new line
+* Return: 0
+*/
+int main(void)
 {
-  char c[] = "Holberton";
-  char *s;
+char c[9] = "Holberton";
+char *s;
 
-  s = c;     
-
-      while(*s != '\0')                                                                                                             
-	{                                                                                                                           
-	  _putchar(*s);                                                                                                                  
-	  s++;                                                                                                                          
-	}                                                                                                                             _putchar('\n');
-      return(0);                                                                                                                    
-    }  
+s = c;
+while (*s != '\0')
+{
+_putchar(*s);
+s++;
+}
+_putchar('\n');
+return (0);
+}
