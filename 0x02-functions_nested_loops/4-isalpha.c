bbf7542cf62453eca903d4ec497623a8aaf338a0
Mwovi Kelvin <ngusyakelvin@gmail.com>
2021-06-23 11:11:33 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+* _isalpha - Checks if c is a letter
+* @c : char to check
+*
+* Description: Checks if c is lowercase or uppercase letter
+* Return: returns 1 if lower or upper case, returns 0 otherwise
+*/
+
+int _isalpha(int c)
+{
+if (c > 65 && c <= 98)
+{
+return (1);
+}
+
+
+else if (c > 97 && c <= 122)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
