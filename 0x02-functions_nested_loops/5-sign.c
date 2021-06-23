bdc83d9c657d8251854aafec207abf2ffc812c51
Mwovi Kelvin <ngusyakelvin@gmail.com>
2021-06-23 12:06:59 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "holberton"
+/**
+* print_sign - print sign
+* @n : number to check
+* Description: prints sign of number if positive neg or zero
+* Return: return 0 if zero return 1 if positive return -1 if negative
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar ('*');
+return (1);
+}
+else if (n < 0)
+{
+_putchar ('-');
+return (-1);
+}
+else
+{
+putchar ('0');
+return (0);
+}
+}
