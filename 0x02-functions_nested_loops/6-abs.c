26ccc77c458bd62a7153d1cd3acb099cd318fc27
Mwovi Kelvin <ngusyakelvin@gmail.com>
2021-06-23 12:23:10 -0700

    _abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+/**
+* _abs - find absolute value
+* @n : number to test
+* Description: find absolute value of an int
+* Return: returns absolute value of n
+*/
+
+int _abs(int n)
+{
+if (n >= 0)
+{
+return (n);
+}
+return (-n);
+}
