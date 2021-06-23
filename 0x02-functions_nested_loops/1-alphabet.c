de4943bcea8b507f57a9bdab7764cd380b197f92
Mwovi Kelvin <ngusyakelvin@gmail.com>
2021-06-23 04:50:00 -0700

    printsthe alphabet, in lowercase, followed by a new line.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+* main - check the code for ALX School students.
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+void print_alphabet(void);
+char b;
+for (b = 'a'; b <= 'z'; b++)
+{
+_putchar(b);
+}
+_putchar('\n');
+return (0);
+}
