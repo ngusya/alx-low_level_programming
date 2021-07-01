a8c338362f474836f0b712ed1bafec22ca20d679
Mwovi Kelvin <ngusyakelvin@gmail.com>
2021-07-01 11:34:54 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+/**
+* _strcmp - compare 2 strings
+*@s1: string 1
+*@s2: string 2
+*
+* Return: Description of the returned value
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+while (*s1)
+{
+if (*s1 != *s2)
+{
+break;
+}
+
+s1++;
+s2++;
+}
+  
+return (*(const unsigned char *)s1 - *(const unsigned char *)s2);
+}
