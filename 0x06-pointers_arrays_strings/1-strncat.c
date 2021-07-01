30a788c7b06611d6c2304c6e4baf441619ae5052
Mwovi Kelvin <ngusyakelvin@gmail.com>
2021-07-01 12:49:29 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,27 +1,24 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
-* _strncat - Concat 2 strings with limit
-* @dest: dest string
-* @src: src string
-* @n: memory limit for src
-*
-* Return: pointer to dest
+* *_strncat - concatenates n bytes of two strings
+* @dest: pointer destination
+* @src: pointer source
+* @n: number of bytes
+* Return: void
 */
 
 char *_strncat(char *dest, char *src, int n)
 {
-int i, j;
 
-for (i = 0; dest[i] != '\0'; i++)
-{
-continue;
-}
+int dest_len, i;
+for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
+;
 
-for (j = 0; src[j] != '0' && j < n; j++)
-{
-dest[i + j] = src[j];
-}
-dest[i + j] = '\0';
+for (i = 0; i < n && src[i] != '\0'; i++)
+dest[dest_len + i] = src[i];
+
+/*should end with a end of string char*/
+dest[dest_len + i] = '\0';
+
 return (dest);
 }
