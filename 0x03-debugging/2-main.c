2943319ed4e2b837c1b9c70ecaafb13333cd386e
Mwovi Kelvin <ngusyakelvin@gmail.com>
2021-07-13 09:01:53 -0700

    Third commit

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -11,9 +11,9 @@ int main(void)
 int a, b, c;
 int largest;
 
-a = 972;
-b = -98;
-c = 0;
+a =-98;
+b = 0;
+c = 978;
 
 largest = largest_number(a, b, c);
 
@@ -21,9 +21,5 @@ largest = largest_number(a, b, c);
 if(a >= b && a >= c)
 printf("%d is the largest number\n", largest);
 
-else if (b >= a && b >= c)
-printf("%d is the largest number\n", largest);
-else
-printf ("%d is the largest number\n", largest);
 return (0);
 }
