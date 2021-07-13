fbd7be20744ec0bb9b39dce6a1dc1204b004b7ec
Mwovi Kelvin <ngusyakelvin@gmail.com>
2021-07-13 02:27:08 -0700

    First commit

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,16 +1,22 @@
-#include "holberton.h"
-
-/**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
-
-int main(void)
-{
-int i;
-
-i = 98;
-positive_or_negative(i);
-
-return (0);
+#include <stdlib.h>                                                                                         
+#include <time.h>                                                                                                         
+#include <stdio.h>                                                                                                        
+/**                                                                                                                       
+ * main - main block                                                                                                       
+ * Description: Get a random number and print the number                                                                   
+ * and if it is positive, negative, or zero                                                                                
+ * Return: 0                                                                                                               
+ */                                                                                                                        
+int main(void)                                                                                                            
+{                                                                                                                         
+  int n;                                                                                                                    
+  srand(time(0));                                                                                                           
+  n = rand() - RAND_MAX / 2;                                                                                                
+  if (n > 0)                                                                                                                
+    printf("%i is positive\n", n);                                                                                            
+  else if (n < 0)                                                                                                           
+    printf("%i is negative\n", n);                                                                                            
+  else                                                                                                                      
+    printf("%i is zero\n", n);                                                                                                
+  return (0);                                                                                                               
 }
