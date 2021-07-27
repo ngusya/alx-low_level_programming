bd1d0fa5f50845badeafaa292c0a688c6d042882
Mwovi Kelvin <ngusyakelvin@gmail.com>
2021-07-27 14:57:12 -0700

    All commits

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,13 @@
-#ifndef DOG_HEADER
-#define DOG_HEADER
+#ifndef DOG_H
+#define DOG_H
 
 /**
-* struct dog - structure of a dog
-* @name: name of dog
-* @age: age of dog
-* @owner: owner of dog
+* struct dog - structure definition of a dog
+* @name: character string
+* @age: integer
+* @owner: character string
 */
+
 typedef struct dog
 {
 char *name;
@@ -18,5 +19,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 #endif
