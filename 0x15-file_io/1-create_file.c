3e4d9e227ed2e87312180616a8e354855dfafd74
Mwovi Kelvin <ngusyakelvin@gmail.com>
2021-08-09 13:45:24 -0700

    Second commit

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+*  create_file - create a file and write in it
+* @filename:file to be created
+* @text_content: content of the file created
+* Return: 1 or -1;
+*/
+int create_file(const char *filename, char *text_content)
+{
+int f, len, w;
+
+if (filename == NULL)
+return (-1);
+
+f = open(filename, O_CREAT | O_RDWR | O_TRUNC, 00600);
+if (f == -1)
+{
+return (-1);
+}
+if (text_content != NULL)
+{
+for (len = 0; text_content[len] != '\0'; len++)
+;
+w = write(f, text_content, len);
+}
+if (w == -1)
+{
+return (-1);
+}
+close(f);
+return (1);
+}
