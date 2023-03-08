05a002cd821b4a93191c242a8d1a901a240faf37
root <root@Ubuntu.myguest.virtualbox.org>
2023-03-08 13:08:59 +0300

    Fixed Betty

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,23 +12,18 @@
 
 char *create_array(unsigned int size, char c)
 {
-  unsigned int a;
-  char *s;
+unsigned int a;
+char *s;
 
-  if(size == 0)
-    {
-      return (NULL);
-    }
-  s = malloc(size * sizeof(*s));
-  if(s == NULL)
-    {
-      return (NULL);
-    }
-  for (a = 0; a< size; a++)
-    {
-      s[a] = c;
-    }
-  return (s);
+if (size == 0)
+return (NULL);
 
-  free(s);
+s = malloc(size * sizeof(*s));
+if (s == NULL)
+return (NULL);
+for (a = 0; a < size; a++)
+s[a] = c;
+return (s);
+
+free(s);
 }
