e4764a872f73e75312701e57c20d84634e787c26
Ubuntu <ubuntu@Ubuntu.myguest.virtualbox.org>
2023-11-30 14:51:33 +0300

    betty fix task 4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,20 +1,17 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
-/*
-*@ free_grid(int **grid, int height):frees a 2 dimensional grid
-*@grid: the address of the two dimensional grid
-*@width: width of the grid
-*@height: height of the grid
-*Return:
-*/
+/**
+ * free_grid - free up a 2d grid
+ * @grid: double pointer 2d grid
+ * @height: height of grid
+ */
+
 void free_grid(int **grid, int height)
 {
-int i;
-for (i = 0; i < height; i++)
-{
-free(grid[i]);
-}
-free(grid);
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
 }
